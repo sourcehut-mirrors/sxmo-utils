94fc8cd5880f21ddc7920a6445f9c736688265b7
Duje Mihanović <duje.mihanovic@skole.hr>
2024-12-28 22:28:48 +0100

    deviceprofile: Extend samsung,coreprimevelte configuration
    Increase the Sway scale to 1.25, widen the brightness range, disable
    modem functionality (because the modem is not yet supported) and
    explicitly define the power button.
    
    Signed-off-by: Duje Mihanović <duje.mihanovic@skole.hr>
    Signed-off-by: Aren Moynihan <aren@peacevolution.org>

--- scripts/deviceprofiles/sxmo_deviceprofile_samsung,coreprimevelte.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_samsung,coreprimevelte.sh
@@ -4,3 +4,7 @@
 # Copyright 2023 Sxmo Contributors
 
 export SXMO_VOLUME_BUTTON="1:1:gpio-keys"
+export SXMO_POWER_BUTTON="0:0:88pm886-onkey"
+export SXMO_NO_MODEM=1
+export SXMO_MIN_BRIGHTNESS=0 # can go all the way down
+export SXMO_SWAY_SCALE="1.25"
