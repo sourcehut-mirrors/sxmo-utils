b9821b9342a9da4115c50726e68fba61ad9609ec
magdesign <hallo@magdesign.ch>
2025-02-05 21:15:09 +0100

    sxmo_deviceprofile_xiaomi,beryllium.sh: was broken, get it working on both panel variants
    Closes: https://todo.sr.ht/~mil/sxmo-tickets/616
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

--- scripts/deviceprofiles/sxmo_deviceprofile_xiaomi,beryllium.sh
+++ scripts/deviceprofiles/sxmo_deviceprofile_xiaomi,beryllium.sh
@@ -3,8 +3,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright 2022 Sxmo Contributors
 
-# TODO: change to output display
-export SXMO_MONITOR="0:0:Novatek_NT36XXX_Touchscreen"
+export SXMO_MONITOR="DSI-1"
 export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
 export SXMO_VOLUME_BUTTON="1:1:gpio-keys 0:0:pm8941_resin"
 export SXMO_SWAY_SCALE="3"
