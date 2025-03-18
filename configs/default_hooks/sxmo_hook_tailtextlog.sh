sourcehut-mirrors/sxmo-utils

commit 8894f2a4e2776039afc701900e65c756e09616c3

Try to simplify the terminal wrapping
The ending wrapping with sxmo_terminal.sh is ugly and difficult to
extend. I struggle in my own hook to extend it so I guess it could
probably is made better.

configs/default_hooks/sxmo_hook_tailtextlog.sh
@@ -4,13 +4,18 @@
 
 # This hook displays the sms log for a numbers passed as $1
 
+LOGDIRNUM="$1"
+
+# Wrap this in a terminal if not already done
+if [ -z "$TERMNAME" ]; then
+	TERMNAME="$LOGDIRNUM SMS"
+	export TERMNAME
+	exec sxmo_terminal.sh "$0" "$@"
+fi
+
 # include common definitions
 # shellcheck source=scripts/core/sxmo_common.sh
 . sxmo_common.sh
-
-LOGDIRNUM="$1"
-TERMNAME="$LOGDIRNUM SMS"
-export TERMNAME
 
 # If it's already open, switch to it.
 if [ "$SXMO_WM" = "sway" ] && [ -z "$SSH_CLIENT" ]; then
@@ -46,5 +51,5 @@ COLORSEDCMD="s#^\(Sent.*\)#$(tput setaf "$SENT_COLOR")\1$(tput op)#;s#^\(Receive
 # Replace phone numbers in the filename with contacts from contact book
 CONTACTSSEDCMD="$(mkcontactssedcmd)"
 
-sxmo_terminal.sh sh -c "tail -n9999 -f \"$SXMO_LOGDIR/$LOGDIRNUM/sms.txt\" |\
-	sed -e \"$CONTACTSSEDCMD\" -e \"$DATESEDCMD\" -e \"$COLORSEDCMD\""
+tail -n9999 -f "$SXMO_LOGDIR/$LOGDIRNUM/sms.txt" |\
+	sed -e "$CONTACTSSEDCMD" -e "$DATESEDCMD" -e "$COLORSEDCMD"

commit e9489024378b61e226de8e488b88e7e166c2c1f2

tailtextlog: only attempt to focus a window when we're going to open a terminal window
This fixes a bug where a terminal would be launched, which would then
detect and focus its self, instead of tailing the log file. This would
result in the terminal window closing as soon as soon as it opened, and
focused it's self, which made it impossible to read logs.

Fixes: 8894f2a4 Try to simplify the terminal wrapping
Signed-off-by: Aren Moynihan <aren@peacevolution.org>
Signed-off-by: Willow Barraco <contact@willowbarraco.fr>

configs/default_hooks/sxmo_hook_tailtextlog.sh
@@ -8,6 +8,14 @@ LOGDIRNUM="$1"
 
 # Wrap this in a terminal if not already done
 if [ -z "$TERMNAME" ]; then
+	# If it's already open, switch to it.
+	if [ "$SXMO_WM" = "sway" ] && [ -z "$SSH_CLIENT" ]; then
+		regesc_termname="$(echo "$TERMNAME" | sed 's|+|\\+|g')"
+		if swaymsg -q "[title=\"^$regesc_termname\$\"]" focus; then
+			exit 0
+		fi
+	fi
+
 	TERMNAME="$LOGDIRNUM SMS"
 	export TERMNAME
 	exec sxmo_terminal.sh "$0" "$@"
@@ -16,14 +24,6 @@ fi
 # include common definitions
 # shellcheck source=scripts/core/sxmo_common.sh
 . sxmo_common.sh
-
-# If it's already open, switch to it.
-if [ "$SXMO_WM" = "sway" ] && [ -z "$SSH_CLIENT" ]; then
-	regesc_termname="$(echo "$TERMNAME" | sed 's|+|\\+|g')"
-	if swaymsg -q "[title=\"^$regesc_termname\$\"]" focus; then
-		exit 0
-	fi
-fi
 
 mkcontactssedcmd() {
 	pnc find "$LOGDIRNUM" | while read -r NUMBER; do