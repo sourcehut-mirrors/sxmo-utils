001bd62e0eb66b49d64bcc0e411f7908e48a8968
Doni Halim <donihaalim@gmail.com>
2025-04-01 01:17:15 +0700

    Add deviceprofile for Xiaomi Mi A2 Lite/Xiaomi Redmi 6 Pro

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_xiaomi,daisy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# xiaomi mi a2 lite / xiaomi redmi 6 pro
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2025 Sxmo Contributors
+
+export SXMO_MONITOR="DSI-1"
+export SXMO_POWER_BUTTON="0:0:pm8941_pwrkey"
+export SXMO_VOLUME_BUTTON="1:1:gpio-keys 0:0:pm8941_resin"
+# export SXMO_VIBRATE_DEV=""
+# export SXMO_VIBRATE_STRENGTH=""
+export SXMO_SWAY_SCALE="3"
