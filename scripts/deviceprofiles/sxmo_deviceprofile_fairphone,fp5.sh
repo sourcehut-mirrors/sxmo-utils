f1397898e27c43cd17d4e552f8544c9d19336f05
Ferass El Hafidi <funderscore@postmarketos.org>
2025-04-25 17:46:20 +0000

    Add deviceprofile for Fairphone 5
    pmOS wiki page:
    <https://wiki.postmarketos.org/wiki/Fairphone_5_(fairphone-fp5)>
    
    Signed-off-by: Ferass El Hafidi <funderscore@postmarketos.org>
    Signed-off-by: Maarten van Gompel <proycon@anaproy.nl>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_fairphone,fp5.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Fairphone 5
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2025 Sxmo Contributors
+
+export SXMO_TOUCHSCREEN_ID="0:0:Goodix_Berlin_Capacitive_TouchScreen"
+export SXMO_POWER_BUTTON="0:0:pmic_pwrkey"
+export SXMO_VOLUME_BUTTON="1:1:gpio-keys 0:0:pmic_resin"
+export SXMO_SWAY_SCALE="3.0"
