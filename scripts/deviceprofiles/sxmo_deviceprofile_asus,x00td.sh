6f46ac4c77565a738d8bb966a135593024f01769
Kevin Widjaja <kevin.widjaja21@gmail.com>
2025-06-21 18:23:56 +0800

    Add device profile for Asus Zenfone Max Pro M1
    - Increase ui scaling to 2.5
    - Change minimum brightness to 1, the lowest value before the screen turn off
    - Set the correct path for haptic device
    
    Signed-off-by: Kevin Widjaja <kevinwidjaja_dev@proton.me>
    (cherry picked from commit 8c9f00384e7ff7c4eda73d8abdaaef7dbbcbc04e)
    (cherry picked from commit 64d10e3cef764f1ddd75b5684917e22984b9781b)
    (cherry picked from commit 8d2c2e161496b5ea24ed7dd57e1278713a61306c)
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_asus,x00td.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2025 Sxmo Contributors
+
+export SXMO_SWAY_SCALE="2.5"
+export SXMO_MIN_BRIGHTNESS=1
+export SXMO_VIBRATE_DEV="/dev/input/by-path/platform-800f000.spmi-platform-800f000.spmi:pmic@1:vibrator@c000-event"
