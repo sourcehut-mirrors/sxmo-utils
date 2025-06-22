7a9caa44d4eb63990cf4e558cd0f29e24f886faf
Kevin Widjaja <kevin.widjaja21@gmail.com>
2025-06-22 17:29:37 +0800

    Add device profile for Google Nexus 10 (samsung-manta)
    - Increase ui scaling to 2.5
    - Change minimum brightness to 1, the lowest value before the screen turn off
    - Set SXMO_WOFI_SMALLSCREEN to 0 for 10 inch tablet
    - Add date to status bar
    
    Signed-off-by: Kevin Widjaja <kevinwidjaja_dev@proton.me>
    (cherry picked from commit ebf4b26625e6efce05b11a76e52ec3d80361341a)
    (cherry picked from commit c18c962f52976f1f99d5e5e80f044618934b5aa9)
    (cherry picked from commit d64961da13b9195a0c56547248968ff24f756b7c)
    Signed-off-by: Anjandev Momi <anjan@momi.ca>

---
+++ scripts/deviceprofiles/sxmo_deviceprofile_samsung,nexus10-manta.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2025 Sxmo Contributors
+# Google Nexus 10 (samsung-manta)
+
+export SXMO_SWAY_SCALE="2.5"
+export SXMO_MIN_BRIGHTNESS=1
+export SXMO_STATUS_DATE_FORMAT="%Y-%m-%d %H:%M"
+export SXMO_WOFI_SMALLSCREEN=0
