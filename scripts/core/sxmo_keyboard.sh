sourcehut-mirrors/sxmo-utils

commit c8e9fb6a44fe11a73d538081b569479912dc188d

sxmo_keyboard.sh: switch to using sxmo_jobs.sh to manage the keyboard.
This will achieve 2 things:

1 - Reduce pidof calls (which are taxing on the system from what Aren has informed
me.

2 - Allow more than just the keyboard program to be set via $KEYBOARD. This will
be useful for i3 using svkbd as it needs the -d switch to dock.

Signed-off-by: hazardchem <hazardchem@disroot.org>
Signed-off-by: Aren Moynihan <aren@peacevolution.org>

scripts/core/sxmo_keyboard.sh
@@ -12,7 +12,7 @@ isopen() {
 	fi
 	case "$KEYBOARD" in
 		'onboard') dbus-send --type=method_call --print-reply --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.freedesktop.DBus.Properties.Get string:"org.onboard.Onboard.Keyboard" string:"Visible" || exit 0 ;;
-		*) pidof "$KEYBOARD" > /dev/null ;;
+		*) sxmo_jobs.sh running sxmo_keyboard > /dev/null ;;
 	esac
 }
 
@@ -24,9 +24,7 @@ open() {
 		case "$KEYBOARD" in
 			'onboard') dbus-send --type=method_call --print-reply --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Show ;;
 			*)
-				#Note: KEYBOARD_ARGS is not quoted by design as it may includes a pipe and further tools
-				# shellcheck disable=SC2086
-				isopen || eval "$KEYBOARD" $KEYBOARD_ARGS >> "${XDG_STATE_HOME:-$HOME}"/sxmo.log 2>&1 &
+				isopen || sxmo_jobs.sh start --group sxmo_keyboard sh -c "$KEYBOARD $KEYBOARD_ARGS" &
 			;;
 		esac
 	fi
@@ -36,7 +34,7 @@ close() {
 	if [ -n "$KEYBOARD" ]; then # avoid killing everything !
 		case "$KEYBOARD" in
 			'onboard') dbus-send --type=method_call --print-reply --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Hide ;;
-			*) pkill -f "$KEYBOARD" ;;
+			*) sxmo_jobs.sh stop sxmo_keyboard ;;
 		esac
 	fi
 }

commit 1b53dcfc9676e9dd1d58ea7ba6029698a2b5d309

sxmo_keyboard.sh: fix toggle function
close() no longer returns a non-zero status if the keyboard was already
closed, so we need to explicitly check if it's open in order for toggle
to work.

Original change by hazardchem, rebased by aren.

Fixes: c8e9fb6a ("sxmo_keyboard.sh: switch to using sxmo_jobs.sh to manage the keyboard.")
Link: https://lists.sr.ht/~mil/sxmo-devel/patches/59330
Signed-off-by: hazardchem <hazardchem@disroot.org>
Signed-off-by: Aren Moynihan <aren@peacevolution.org>

scripts/core/sxmo_keyboard.sh
@@ -24,7 +24,7 @@ open() {
 		case "$KEYBOARD" in
 			'onboard') dbus-send --type=method_call --print-reply --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Show ;;
 			*)
-				isopen || sxmo_jobs.sh start --group sxmo_keyboard sh -c "$KEYBOARD $KEYBOARD_ARGS" &
+				sxmo_jobs.sh start --group sxmo_keyboard sh -c "$KEYBOARD $KEYBOARD_ARGS" &
 			;;
 		esac
 	fi
@@ -40,7 +40,8 @@ close() {
 }
 
 if [ "$1" = "toggle" ]; then
-	close || open
+	#shellcheck disable=SC2015
+	isopen && close || open
 elif [ "$1" = "close" ]; then
 	if isopen; then
 		close
@@ -48,5 +49,5 @@ elif [ "$1" = "close" ]; then
 elif [ "$1" = "isopen" ]; then
 	isopen || exit 1
 else
-	open
+	isopen || open
 fi