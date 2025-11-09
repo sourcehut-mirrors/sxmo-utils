sourcehut-mirrors/sxmo-utils

commit e74c348790bc9e27a789c43750feee5b990f49b7

sxmo_hook_contextmenu.sh: fix identifying vte3-gtk4

configs/default_hooks/sxmo_hook_contextmenu.sh
@@ -207,7 +207,7 @@ case "$WMCLASS" in
 		"
 		WINNAME=St
 		;;
-	*foot*|st|st-*|*terminal*|org.gnome.vte.application|*alacritty*)
+	*foot*|st|st-*|*terminal*|org.gnome.vte.app.gtk4|*alacritty*)
 
 		# These git commands only launch the editor.
 		case "$WMNAME" in
@@ -483,7 +483,7 @@ case "$WMCLASS" in
 					"
 					WINNAME=Foot
 					;;
-				*terminal*|org.gnome.vte.application)
+				*terminal*|org.gnome.vte.app.gtk4)
 					CHOICES="$icon_kbd Hotkeys ^ 0 ^ sxmo_appmenu.sh sthotkeys"
 					WINNAME=Terminal
 					;;

commit 8966c46acad5f37f3fc2b75e1222a5584c7c9d2a

sxmo_hook_contextmenu.sh: add back org.gnome.vte.application
This was removed when adding support for vte3-gtk4, add it back in case
users have an old version of vte.

Fixes: e74c3487 sxmo_hook_contextmenu.sh: fix identifying vte3-gtk4
Signed-off-by: Aren Moynihan <aren@peacevolution.org>

configs/default_hooks/sxmo_hook_contextmenu.sh
@@ -211,7 +211,7 @@ case "$WMCLASS" in
 		"
 		WINNAME=St
 		;;
-	*foot*|st|st-*|*terminal*|org.gnome.vte.app.gtk4|*alacritty*)
+	*foot*|st|st-*|*terminal*|org.gnome.vte.application|org.gnome.vte.app.gtk4|*alacritty*)
 
 		# These git commands only launch the editor.
 		case "$WMNAME" in
@@ -487,7 +487,7 @@ case "$WMCLASS" in
 					"
 					WINNAME=Foot
 					;;
-				*terminal*|org.gnome.vte.app.gtk4)
+				*terminal*|org.gnome.vte.application|org.gnome.vte.app.gtk4)
 					CHOICES="$icon_kbd Hotkeys ^ 0 ^ sxmo_appmenu.sh sthotkeys"
 					WINNAME=Terminal
 					;;