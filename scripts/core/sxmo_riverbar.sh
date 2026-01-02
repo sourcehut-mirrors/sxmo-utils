d92b62a70cd8be8cad71b2c42a649a8639c581bb
hazardchem <hazardchem@disroot.org>
2026-01-02 22:56:56 +1000

    sxmo_riverbar.sh: remove information from bar.
    Currently sandbar always displays the mode and other status information.
    This obscures the information from sxmobar.
    
    Signed-off-by: hazardchem <hazardchem@disroot.org>

--- scripts/core/sxmo_riverbar.sh
+++ scripts/core/sxmo_riverbar.sh
@@ -4,6 +4,9 @@ sxmobar -o plain -w | \
 	stdbuf -o0 tr '\n' '\0' | \
 	xargs -0 -n1 printf "all status %s\n" | \
 	sandbar -font "Sxmo" -hide-vacant-tags \
+		-hide-normal-mode \
+		-no-status-commands \
+		-no-layout \
 		-active-fg-color 282a36 -active-bg-color ff4971 \
 		-inactive-fg-color fdfdfd -inactive-bg-color 282a36 \
 		-title-fg-color fdfdfd -title-bg-color 282a36 \
